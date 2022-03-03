c5bd7b6bf5471df95794b628d6db46e6d5e0c6eb
alliehaberlin <70352156+alliehaberlin@users.noreply.github.com>
2022-03-03 12:19:25 -0500

    hw4 update

--- hw4.sql
+++ hw4.sql
@@ -15,8 +15,22 @@ CREATE TABLE orders (
     PRIMARY KEY (id),
     FOREIGN KEY (customerId) REFERENCES customers(id)
 );
-INSERT INTO customers (firstname, lastname, store) VALUES ('john', 'painter', 'artists');
-INSERT INTO customers (firstname, lastname, store) VALUES ('maeve', 'musician', 'artists');
+CREATE TABLE addons (
+    id int NOT NULL UNIQUE AUTO_INCREMENT,
+    product varchar(20),
+    cost int,
+    customerId int,
+    PRIMARY KEY (id),
+    FOREIGN KEY (customerId) REFERENCES customers(id)
+);
+CREATE TABLE customer_order_association (
+    id int NOT NULL UNIQUE AUTO_INCREMENT,
+    orderId int NOT NULL,
+    customerId int NOT NULL,
+    PRIMARY KEY (id)
+);
+INSERT INTO customers (firstname, lastname, store) VALUES ('vincent', 'painter', 'artists');
+INSERT INTO customers (firstname, lastname, store) VALUES ('john', 'musician', 'artists');
 
 INSERT INTO orders (product, cost, customerId) VALUES ('canvas', '10',1);
 INSERT INTO orders (product, cost, customerId) VALUES ('brush', '2',1);
@@ -25,3 +39,7 @@ INSERT INTO orders (product, cost, customerId) VALUES ('drumstick', '6',2);
 
 select * from orders;
 select * from customers;
+select * from customer_order_association;
+
+INSERT INTO customer_order_association (orderid, customerid) VALUES (3, 3);
+
